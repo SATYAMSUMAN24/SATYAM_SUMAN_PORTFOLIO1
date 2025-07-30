f3c7a27a3b9eac8da4e0bcca256340f8b562da79
SATYAM SUMAN <146647625+SATYAMSUMAN24@users.noreply.github.com>
2025-07-30 21:35:08 +0530

    references.js

--- assets/js/references.js
+++ assets/js/references.js
@@ -16,7 +16,7 @@ const references = [
         sl2:2,
         name2 : "Aditya kumar",
         designation2 : "Senior Analyst, Capgemini", 
-        image2: "/assets/images/references-page/reviewphoto2",
+        image2: "/assets/images/references-page/reviewphoto2.png",
         message2 : "Satyam has the best grasping skills I have seen in a new developer. I got to know him through GSSOC'2021 when I mentored him, and he made an exceptional growth during the competition. He made some incredible contributions and has good experience with Open Source.",
         
         
@@ -25,7 +25,7 @@ const references = [
     {
         sl1:3,
         name1 : "Sudipta Panda",
-        designation1 : "/assets/images/references-page/reviewphoto1",
+        designation1 : "/assets/images/references-page/reviewphoto1.png",
         image1 : "https://media-exp1.licdn.com/dms/image/C5603AQGEV-kzawPxjw/profile-displayphoto-shrink_800_800/0/1588602102622?e=1651708800&v=beta&t=8nxhFoR-WB4OKVltyYaVjmRpig_kQcvFsoa-eLjJPBQ",
         message1 : "Satyam and I have worked on several projects. Apart from technical skills, he also has good soft skills. He has good experience with debugging and has excellent leadership qualities.",
         
@@ -33,7 +33,7 @@ const references = [
         sl2:4,
         name2 : "Arnab Suman",
         designation2 : "Data Scientist, Flipkart",
-        image2 : "/assets/images/references-page/reviewphoto4",
+        image2 : "/assets/images/references-page/reviewphoto4.png",
         message2 : "Satyam and I worked together during a hackathon and the thing that surprised me the most about him was his knowledge of fundamentals. He offered sincere help to the team throughout the competition. He is an impressive problem solver who is able to address complex issues strategically and confidently.",
   },
 ];
